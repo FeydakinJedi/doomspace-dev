419baf0aea80bb6e46e6c4ca0ae496d3206aa809
FeydakinJedi <acdn01@gmail.com>
2024-09-06 13:30:12 -0400

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const BOILERPLATE = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+BOILERPLATE.abilities = {
+  str: 'BOILERPLATE.Ability.Str.long',
+  dex: 'BOILERPLATE.Ability.Dex.long',
+  con: 'BOILERPLATE.Ability.Con.long',
+  int: 'BOILERPLATE.Ability.Int.long',
+  wis: 'BOILERPLATE.Ability.Wis.long',
+  cha: 'BOILERPLATE.Ability.Cha.long',
+  san: 'BOILERPLATE.Ability.San.long',
+};
+
+BOILERPLATE.abilityAbbreviations = {
+  str: 'BOILERPLATE.Ability.Str.abbr',
+  dex: 'BOILERPLATE.Ability.Dex.abbr',
+  con: 'BOILERPLATE.Ability.Con.abbr',
+  int: 'BOILERPLATE.Ability.Int.abbr',
+  wis: 'BOILERPLATE.Ability.Wis.abbr',
+  cha: 'BOILERPLATE.Ability.Cha.abbr',
+  san: 'BOILERPLATE.Ability.San.abbr',
+};
